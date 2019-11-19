f6f97a82cd476cb0fe1325ef18acb35bc557db14
root <root@localhost.localdomain>
2019-11-19 23:44:02 +0800

    airflow

---
+++ yang.py
@@ -0,0 +1,44 @@
+from datetime import timedelta
+
+import airflow
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+default_args = {
+    'owner': 'Airflow',
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(2),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'trigger_rule': u'all_success'
+}
+
+dag = DAG(
+    'yang',
+    default_args=default_args,
+    description='A simple yang DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+t1 = BashOperator(
+    task_id='build',
+    bash_command='curl http://admin:11a712430f8f1e28fa6642992fffaa1daa@localhost:8080/job/pp3/build -X POST',
+    dag=dag,
+)
+
